39be60c838428dc482d22254efd01a51c6ec007c
runningship <runningship@gmail.com>
2015-09-13 22:56:04 +0800

    food

---
+++ src/main/java/com/youwei/food/entity/Nutrient.java
@@ -0,0 +1,24 @@
+package com.youwei.food.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Nutrient {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Integer id;
+	
+	public Integer foodId;
+	
+	public String name;
+	
+	public Float value;
+	
+	public String unit;
+	
+	public String leibie;
+}
